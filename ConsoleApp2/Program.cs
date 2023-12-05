e47feacf826252a29896793cc07865e903366a2e
{{EvilAtomic}} <{{artemasarkisan598@gmail.com}}>
2023-12-05 11:40:59 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+abstract class GraphicPrimitive
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public abstract void Draw();
+    public abstract void Move(int x, int y);
+    public abstract void Scale(float factor);
+}
+
+class Circle : GraphicPrimitive
+{
+    public int Radius { get; set; }
+
+    public override void Draw()
+    {
+        Console.WriteLine($"Drawing Circle at ({X}, {Y}) with Radius {Radius}");
+    }
+
+    public override void Move(int x, int y)
+    {
+        X += x;
+        Y += y;
+    }
+
+    public override void Scale(float factor)
+    {
+        Radius = (int)(Radius * factor);
+    }
+}
+
+class Rectangle : GraphicPrimitive
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public override void Draw()
+    {
+        Console.WriteLine($"Drawing Rectangle at ({X}, {Y}) with Width {Width} and Height {Height}");
+    }
+
+    public override void Move(int x, int y)
+    {
+        X += x;
